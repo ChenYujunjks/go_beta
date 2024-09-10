826b4ab6f0423fa0bf40a5d4ca29ffd152e98cee
Bruce Chen <aaaeeettt124@126.com>
2024-09-11 00:08:12 +0800

    new file sys: :

--- gointro/main.go
+++ gointro/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	gor "beta/goroutine"
-	utils "beta/pkg1"
+	er "beta/error"
+	func_intro "beta/func"
+	interf_test "beta/interface"
+	struct_l "beta/struct"
 	"bufio"
 	"fmt"
 	"os"
@@ -12,16 +14,22 @@ import (
 
 func main() {
 
+	fmt.Println("|您现在位于模块1：简单学习---------|")
+	fmt.Println("|请输入一个数字来选择要运行的函数: |")
+	fmt.Println("|1: 运行匿名函数                   |")
+	fmt.Println("|2: 运行 runtime 查看CPU核心       |")
+	fmt.Println("|3: 查看Day1 func                  |")
+	fmt.Println("|4: 查看Day2 error                 |")
+	fmt.Println("|5: 查看Day3 interface             |")
+	fmt.Println("|6: 查看Day4 struct                |")
+	fmt.Println("|7: 查看defer的用法                 |")
+	fmt.Println("|0: 退出                           |")
+	fmt.Println("|----------------------------------|")
+
+	// 获取用户输入
 	reader := bufio.NewReader(os.Stdin)
 	for {
-		fmt.Println("|----------------------------------|")
-		fmt.Println("|请输入一个数字来选择要运行的模块: |")
-		fmt.Println("|1: 普通                         |")
-		fmt.Println("|2: Goroutine                   |")
-		fmt.Println("|3: tBD      |")
-		fmt.Println("|0: 退出                           |")
-		fmt.Println("|----------------------------------|")
-		// 获取用户输入
+		fmt.Println("---------------------------------->>")
 		fmt.Print("请输入选项: ")
 		input, _ := reader.ReadString('\n')
 		input = strings.TrimSpace(input) // 去除空白字符
@@ -36,16 +44,25 @@ func main() {
 		// 根据用户输入的数字来调用相应的函数
 		switch choice {
 		case 1:
-			utils.Ask_one()
+			Anon_func()
 		case 2:
-			gor.Ask_goroutine()
+			Go_runtime()
 		case 3:
-			fmt.Println("TBD!")
+			func_intro.Day1_Func()
+		case 4:
+			er.Error_F()
+		case 5:
+			interf_test.Intereiafoewj()
+		case 6:
+			struct_l.Struct_jjj()
+		case 7:
+			defer_test()
 		case 0:
-			fmt.Println("|--------------退出总程序------------|")
+			fmt.Println("|--------------退出程序------------|")
 			return
 		default:
 			fmt.Println("无效输入，请重新输入")
 		}
 	}
+
 }
