7693df0b397c8903d1a63481690a5b96cf15f83a
Bruce Chen <aaaeeettt124@126.com>
2025-05-18 18:56:23 +0800

    adding new cli

--- cli/main.go
+++ cli/main.go
@@ -7,8 +7,10 @@ import (
 	"time"
 
 	"github.com/briandowns/spinner"
+	"github.com/cheggaaa/pb/v3"
 	"github.com/fatih/color"
 )
+
 
 func main() {
 	if len(os.Args) < 2 {
@@ -16,12 +18,15 @@ func main() {
 		color.Red("这是红色的输出")
 		color.Green("成功！")
 		color.Cyan("信息：%s", "处理完成")
-		fmt.Println("No command provided")
+
 		//briandowns spinner
 		s := spinner.New(spinner.CharSets[14], 100*time.Millisecond) // 可选多种样式
 		s.Start()		
-		time.Sleep(2 * time.Second) // 模拟长任务
+		time.Sleep(2 * time.Second) // 模拟长任务		
+		fmt.Println("No command provided")
 		s.Stop()
+
+		chegg() //cheggaaa pb
 		return
 	}
 
@@ -38,3 +43,14 @@ func main() {
 func printIm() {
 	fmt.Println("Hello, this is the 'im' command")
 }
+func chegg(){
+	count := 100
+    bar := pb.StartNew(count)
+    for i := 0; i < count; i++ {
+        bar.Increment()
+        time.Sleep(20 * time.Millisecond)
+    }
+    bar.Finish()
+}
+
+
